d2ff9bb527b76c46ba43208554200bc1ce37b88a
mohamed <m635379@gmail.com>
2022-12-02 23:13:05 +0200

    learn some thin new

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 const gulp = require("gulp");
-const concat = require("gulp-concat");
-const prefix = require("gulp-autoprefixer");
 
-// Concat, Prefixer Plugin
-gulp.task("css", function () {
-  return gulp
-    .src("./project/*.css")
-    .pipe(prefix("last 2 versions"))
-    .pipe(concat("main.css"))
-    .pipe(gulp.dest("./dist"));
-});
+// Css Task
+function css(cb) {
+  gulp.src("./project/css/");
+
+  // Something...
+  cb();
+}
+
+exports.css = css;
