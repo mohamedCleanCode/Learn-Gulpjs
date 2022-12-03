6428acff6981da21da0744baf1714050a08e9557
mohamed <m635379@gmail.com>
2022-12-03 21:33:13 +0200

    learn sass, compers css file, static-server and watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,40 @@
-const gulp = require("gulp");
+var gulp = require("gulp"),
+  concat = require("gulp-concat"),
+  prefix = require("gulp-autoprefixer"),
+  sass = require("gulp-sass")(require("sass")),
+  pug = require("gulp-pug"),
+  watch = require("gulp-watch");
+
+// Html Task
+gulp.task("html", function () {
+  return gulp
+    .src("./project/index.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("./dist"));
+});
 
 // Css Task
-function css(cb) {
-  gulp.src("./project/css/");
+gulp.task("css", function () {
+  return gulp
+    .src("./project/css/main.scss")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(prefix("last 2 version"))
+    .pipe(concat("main.css"))
+    .pipe(gulp.dest("./dist/css"));
+});
 
-  // Something...
-  cb();
-}
+// JS Task
+gulp.task("js", function () {
+  return gulp
+    .src("./project/js/*.js")
+    .pipe(concat("main.js"))
+    .pipe(gulp.dest("./dist/js"));
+});
 
-exports.css = css;
+// Watch Task
+gulp.task("watch", function () {
+  require("./server");
+  gulp.watch("./project/index.pug", gulp.series("html"));
+  gulp.watch("./project/css/**/*.scss", gulp.series("css"));
+  gulp.watch("./project/js/*.js", gulp.series("js"));
+});
