d1ac77994092b018d54a8667dc5268e5f7cc7c49
mohamed <m635379@gmail.com>
2022-12-06 00:40:29 +0200

    learn livereload and sourcemaps

---
+++ dist/js/main.js
@@ -0,0 +1,5 @@
+var one;
+
+var two;
+
+//# sourceMappingURL=main.js.map
--- gulpfile.js
+++ gulpfile.js
@@ -3,37 +3,47 @@ var gulp = require("gulp"),
   prefix = require("gulp-autoprefixer"),
   sass = require("gulp-sass")(require("sass")),
   pug = require("gulp-pug"),
-  watch = require("gulp-watch");
+  watch = require("gulp-watch"),
+  livereload = require("gulp-livereload"),
+  sourcemaps = require("gulp-sourcemaps");
 
 // Html Task
 gulp.task("html", function () {
   return gulp
     .src("./project/index.pug")
     .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest("./dist"))
+    .pipe(livereload());
 });
 
 // Css Task
 gulp.task("css", function () {
   return gulp
     .src("./project/css/main.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: "compressed" }))
     .pipe(prefix("last 2 version"))
     .pipe(concat("main.css"))
-    .pipe(gulp.dest("./dist/css"));
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(livereload());
 });
 
 // JS Task
 gulp.task("js", function () {
   return gulp
     .src("./project/js/*.js")
+    .pipe(sourcemaps.init())
     .pipe(concat("main.js"))
-    .pipe(gulp.dest("./dist/js"));
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/js"))
+    .pipe(livereload());
 });
 
 // Watch Task
 gulp.task("watch", function () {
   require("./server");
+  livereload.listen();
   gulp.watch("./project/index.pug", gulp.series("html"));
   gulp.watch("./project/css/**/*.scss", gulp.series("css"));
   gulp.watch("./project/js/*.js", gulp.series("js"));
